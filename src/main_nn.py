2050dc88bfdc0d4aee36cbf5ad900083bdb88c72
beiyuouo <beiyu233@gmail.com>
2022-03-10 18:42:01 +0800

    bce

--- src/main_nn.py
+++ src/main_nn.py
@@ -10,6 +10,7 @@ import os
 import sys
 base_path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 sys.path.append(base_path)
+import time
 import argparse
 import numpy as np
 from scipy import signal
@@ -23,9 +24,10 @@ from src.model import EEGNet
 opt = argparse.ArgumentParser()
 opt.add_argument('--data_path', type=str, default='../data/', help='data path')
 opt.add_argument('--result_path', type=str, default='../result/', help='result path')
-opt.add_argument('--batch_size', type=int, default=16, help='batch size')
-opt.add_argument('--epochs', type=int, default=100, help='epochs')
-opt.add_argument('--lr', type=float, default=0.0001, help='learning rate')
+opt.add_argument('--batch_size', type=int, default=4, help='batch size')
+opt.add_argument('--epochs', type=int, default=300, help='epochs')
+opt.add_argument('--lr', type=float, default=0.001, help='learning rate')
+opt.add_argument('--model_path', type=str, default='../model/', help='model path')
 args = opt.parse_args()
 
 
@@ -60,7 +62,7 @@ def get_data(start_idx, end_idx):
 
 
 def train_test(train_data, train_label, test_data, in_channels=30, in_feature=3750):
-    model = EEGNet(classes_num=2, in_channels=in_channels, in_feature=in_feature, dropout=0.5)
+    model = EEGNet(classes_num=1, in_channels=in_channels, in_feature=in_feature, dropout=0.25)
     print(model)
 
     b, a = signal.butter(8, [8 * 2 / 250, 30 * 2 / 250], 'bandpass')
@@ -82,7 +84,7 @@ def train_test(train_data, train_label, test_data, in_channels=30, in_feature=37
 
     # to tensor
     train_X = torch.from_numpy(train_X).float()
-    train_y = torch.from_numpy(train_y).long()
+    train_y = torch.from_numpy(train_y).float()
     test_X = torch.from_numpy(test_X).float()
 
     print(train_X.shape, train_y.shape, test_X.shape)
@@ -94,36 +96,53 @@ def train_test(train_data, train_label, test_data, in_channels=30, in_feature=37
                                                shuffle=True)
 
     # train
-    criterion = nn.CrossEntropyLoss()
+    criterion = nn.BCELoss()
     optimizer = torch.optim.Adam(model.parameters(), lr=args.lr)
-    scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=10, gamma=0.1)
+    milestones = [50, 100, 150, 200, 250]
+    milestones = [a * len(train_loader) for a in milestones]
+    scheduler = torch.optim.lr_scheduler.MultiStepLR(optimizer,
+                                                     milestones=milestones,
+                                                     gamma=0.5)
 
     model.train()
     model = model.cuda()
 
     for epoch in range(args.epochs):
+        running_loss = 0.0
+        accuarcy = 0.0
+        total = 0.0
+
         for i, (inputs, labels) in enumerate(train_loader):
             inputs = inputs.cuda()
             labels = labels.cuda()
 
             outputs = model(inputs)
             # _, preds = torch.max(outputs, 1)
+            # binary cross entropy
+            outputs = torch.sigmoid(outputs)
+            outputs = outputs.view(-1)
             # print(outputs.shape, labels.shape)
             # print(outputs, labels)
-            loss = criterion(torch.sigmoid(outputs), labels)
+            loss = criterion(outputs, labels)
 
             optimizer.zero_grad()
             loss.backward()
+
             optimizer.step()
+            scheduler.step()
 
-            if (i + 1) % 100 == 0:
-                print('Epoch [%d/%d], Step [%d/%d], Loss: %.4f' %
-                      (epoch + 1, args.epochs, i + 1, len(train_loader), loss.item()))
+            running_loss += loss.item()
+            # binary classification
+            outputs = outputs.cpu().detach().numpy()
+            outputs = np.where(outputs > 0.5, 1, 0)
+            labels = labels.cpu().detach().numpy()
+            accuarcy += np.sum(outputs == labels)
+            total += len(labels)
+            # print(accuarcy, total)
 
-        scheduler.step()
-
-        if epoch % 10 == 0:
-            print('Epoch [{}/{}], Loss: {:.4f}'.format(epoch + 1, args.epochs, loss.item()))
+        if epoch % 1 == 0:
+            print('Epoch [{}/{}], Loss: {:.4f} Accuarcy: {:.4f}'.format(
+                epoch + 1, args.epochs, running_loss / len(train_loader), accuarcy / total))
 
     # test
     model.eval()
@@ -133,9 +152,16 @@ def train_test(train_data, train_label, test_data, in_channels=30, in_feature=37
         for i, (inputs) in enumerate(test_X):
             inputs = inputs.cuda()
             outputs = model(inputs)
-            _, preds = torch.max(outputs, 1)
-            test_pred.extend(preds.cpu().numpy().tolist())
+            outputs = torch.sigmoid(outputs)
+            outputs = outputs.view(-1)
+            outputs = outputs.cpu().detach().numpy()
+            outputs = np.where(outputs > 0.5, 1, 0)
+            preds = outputs.tolist()
+            test_pred.extend(preds)
 
+    # save model
+    torch.save(model.state_dict(),
+               os.path.join(args.model_path, f'model_{in_feature}_{int(time.time())}.pt'))
     return test_pred
 
 
